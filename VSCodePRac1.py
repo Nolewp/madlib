c9708d881adeb686fa2547159d61b67de2249510
Nolewp <68575675+Nolewp@users.noreply.github.com>
2021-08-04 19:14:41 -0400

    Add files via upload

---
+++ VSCodePRac1.py
@@ -0,0 +1,10 @@
+#python
+# make me a madlib for some python fun!
+# f string lets you fill in variables in a string 
+
+
+
+noun = input(" Noun: ")
+adj1 = input(" Adj: ")
+verb = input(" Verb: ")
+print(f"Hwllo my {noun}!. How do you do? I am {adj1}. We should go {verb}")
